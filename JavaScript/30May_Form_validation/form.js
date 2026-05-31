f801ef2801a2f24891ac6f8b89a5d5b33275c023
mayurrajput8486 <mayurrajput8486@gmail.com>
2026-05-31 11:13:59 +0530

    Validation

--- JavaScript/30May_Form_validation/form.js
+++ JavaScript/30May_Form_validation/form.js
@@ -41,6 +41,7 @@ function toggleTheme() {
 
     REGEX SYMBOL FOR QUICK GUIDE
     ----------------------------
+    /regEx Pattern /= basic foundation of regEx
     ^               = start of the string
     $               = end of the string
     [a-z]           = any lowercase letter a to z
@@ -54,6 +55,7 @@ function toggleTheme() {
     {3,}            = 3 or more character accepted.
     {3,10}          = Minimum 3 and maximum 10 character allowed
     (?=.*[A-Z])     = in you string must have uppercase character
+    .               = any character
     [^ ]            = Not this character (inside character not allowed in string)
     [a-zA-Z\d@#$%&*]= string having lowercase, uppercase, digit and special character from @,#,$,%,&,*
 
@@ -66,50 +68,100 @@ function toggleTheme() {
 
 */
 let patterns = {
-    fullname : /^[a-zA-Z\s]{2,}$/,
-    username : /^[a-zA-Z0-9_]{4,15}$/,
-    email : /^[^\s@]+@[^\s@]+\.[^\s@]{5,}$/,
+    fullname: /^[a-zA-Z\s]{2,}$/,
+    username: /^[a-zA-Z0-9_]{4,15}$/,
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
     //virat@gmail.com --- user@domain.com
-    phone : /^[6-9]{1}[0-9]{9}$/,
-    password : /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$%&_]){5,10}$/
+    phone: /^[6-9]{1}[0-9]{9}$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$%&_]).{5,}$/
 }
 
 /* This function is called EVERY TIME the user entered data */
-function checkField (id) {
+function checkField(id) {
     //Get the user input and values
     let inputEle = document.getElementById(id)
+
+    //Get user input value and remove extra whitespaces
     let typedValue = inputEle.value.trim()
+
     //Get the error message element
-    let errorInput = document.getElementById(id+'-err')
-    /* 
-    Example : id = "email" ----> "email-err"
-    */
+    let errorInput = document.getElementById(id + '-err')
+
+    //Example : id = "email" ----> "email-err"
+
 
     //Check if the value is valid
+
     /* Start assuming the is not valid */
     let isValid = false;
 
     /* 
-        Specialcase for confirmPassword
+        STEP 2 - Specialcase for confirmPassword
         No regex needed - just check if it match the password filed 
     */
 
-    if(id === 'confirmPassword'){
+    if (id === 'confirmPassword') {
+        //"fullname" === "confirmPassword"          false       else
+        //"email"    === "confirmPassword"          false       else
+        //"username" === "confirmPassword"          false       else
+        //"password" === "confirmPassword"          false       else
+        //'confirmPassword' === 'confirmPassword'   true        if
         let originalpasswordValue = document.getElementById('password').value
         let passwordMatch = typedValue.length > 0 && typedValue === originalpasswordValue
         isValid = passwordMatch
-    }else{
+    } else {
+
+        /* if Above condition is false then - This else part is test the regex pattern  */
+        //pattern[id]   - call every object field 
+        //.test(typeValue) --- test() function return true or false
+        //if pattern is match then true return
+        //if pattern is not match then false return
         let patternMatch = patterns[id].test(typedValue)
         //                 patterns['password'].test(typedValue)
         isValid = patternMatch
     }
 
+    /* 
+        STEP 3 -  Update the input border color
+        green - field valid
+        red   - field invalid 
     
+    */
+
+    if (typedValue.length === 0) {
+        inputEle.classList = ''
+    } else if (isValid) {
+        inputEle.className = 'valid'
+    } else {
+        inputEle.className = 'invalid'
+    }
+
+    /* 
+        classList.add('class')  add class dynamically after click (hum new class create karate hai)    
+        className               assign class (joh pahale se he created hai)
+    */
+
+    /* 
+        STEP 4 - Show and hide error message
+        In form css - 
+        .err-msg{display : none} - by default hidden
+        .err-msg.show {display : block} - 
+    */
+
+    
+    console.log(isValid)
+    if(!isValid && typedValue.length > 0){
+        errorInput.classList.add('show')
+    }else{
+        errorInput.classList.remove('show')
+    }
 
 
+    return isValid;
+}
 
-
-
+/* Submit Form Function */
+function submitForm() {
 
 }
 
